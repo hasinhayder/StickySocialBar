a8c954c994f8d7ec46d0a51875ce8f8ae75a9435
Hasin Hayder <hasin@leevio.com>
2013-07-11 16:26:04 +0600

    Sticky Social Bar

---
+++ sticky.js
@@ -0,0 +1,12 @@
+;
+(function($){
+	var opts;
+	$.fn.jSticky = function(options){
+		var defaults = {};
+		opts = $.extend(defaults, options);
+
+		var w = $(window).width();
+		var h = $(window).width();
+	}
+
+})(jQuery);
